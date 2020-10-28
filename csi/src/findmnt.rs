463d15417391cb715c87cbdbffd0a8fee4c0eb08
Blaise Dias <blaise.dias@mayadata.io>
2020-10-28 18:43:21 +0000

    Review fixes part 2
    Refactor to make it clearer and simpler.

--- csi/src/findmnt.rs
+++ csi/src/findmnt.rs
@@ -20,30 +20,14 @@ struct Filter<'a> {
     value: &'a str,
 }
 
-impl PartialEq<Value> for Filter<'_> {
-    /// Special case the comparison for the source field returned
-    /// by findmnt.
-    fn eq(&self, value: &Value) -> bool {
-        if self.key == SOURCE_KEY {
-            if let Some(strvalue) = value.as_str() {
-                let devpath = convert_findmnt_devicepath(strvalue);
-                if devpath == self.value {
-                    return true;
-                }
-            } else {
-                error!("value for {} is not a string", self.key);
-            }
-        }
-        self.value == value
-    }
-}
-
-/// The source field returned from findmnt and
-///   can be different for the same source on different systems,
-///   for example
+/// Convert a json value of a key-value pair, to a string,
+/// adjusted if required, on the key.
+///
+/// The source field returned from findmnt can be different for
+/// the same source on different systems, for example
 ///   dev[/nvme0n1], udev[/nvme0n1], tmpfs[/nvme0n1], devtmpfs[/nvme0n1]
-///   Convert this to the expected /dev/nvme0n1 and added to the hashmap
-fn convert_findmnt_devicepath(devpath: &str) -> String {
+/// this function converts those values to the expected /dev/nvme0n1
+fn key_adjusted_value(key: &str, value: &Value) -> String {
     lazy_static! {
         static ref RE_UDEVPATH: regex::Regex = regex::Regex::new(
             r"(?x).*\[(?P<device>/.*)\]
@@ -51,13 +35,26 @@ fn convert_findmnt_devicepath(devpath: &str) -> String {
         )
         .unwrap();
     }
-    match RE_UDEVPATH.captures(devpath) {
-        Some(caps) => format!("/dev{}", &caps["device"]),
-        _ => devpath.to_string(),
+
+    // Do NOT do
+    //    let strvalue = value.to_string();
+    // that will return a string delimited with quote characters.
+    let strvalue: String = match value {
+        Value::String(str) => str.to_string(),
+        _ => value.to_string(),
+    };
+    if key == SOURCE_KEY {
+        if let Some(caps) = RE_UDEVPATH.captures(&strvalue) {
+            return format!("/dev{}", &caps["device"]);
+        };
     }
+    strvalue
 }
 
+const KEYS: &[&str] = &[TARGET_KEY, SOURCE_KEY, FSTYPE_KEY];
+
 /// Convert the json map entry to a hashmap of strings
+/// with source, target and fstype key-value pairs only.
 /// The source field returned from findmnt is converted
 /// to the /dev/xxx form if required.
 fn jsonmap_to_hashmap(
@@ -65,27 +62,17 @@ fn jsonmap_to_hashmap(
 ) -> HashMap<String, String> {
     let mut hmap: HashMap<String, String> = HashMap::new();
     for (key, value) in json_map {
-        if let Some(strvalue) = value.as_str() {
-            if key == SOURCE_KEY {
-                hmap.insert(
-                    key.to_string(),
-                    convert_findmnt_devicepath(strvalue),
-                );
-            } else {
-                hmap.insert(key.to_string(), strvalue.to_string());
-            }
-        } else {
-            //FIXME: key:value pairs are discarded if the value is not a
-            // string.
-            error!("value for {} is not a string", key);
+        if KEYS.contains(&key.as_str()) {
+            hmap.insert(key.clone(), key_adjusted_value(key, value));
         }
     }
     hmap
 }
 
 /// This function recurses over the de-serialised JSON returned by findmnt,
-/// finding entries which have key-pair's matching the filter key-pair,
-/// and populates a vector with the values for the item_key.
+/// finding entries which have key-value pair's matching the
+/// filter key-value pair, and populates a vector with those
+/// entries as hashmaps of strings.
 ///
 /// For Mayastor usage the assumptions made on the structure are:
 ///  1. An object has keys named "target" and "source" for a mount point.
@@ -98,38 +85,45 @@ fn filter_findmnt(
     filter: &Filter,
     results: &mut Vec<HashMap<String, String>>,
 ) {
-    if let Some(json_array) = json_val.as_array() {
-        for jsonvalue in json_array {
-            filter_findmnt(&jsonvalue, filter, results);
-        }
-    }
-    if let Some(json_map) = json_val.as_object() {
-        if let Some(value) = json_map.get(filter.key) {
-            if filter == value {
-                results.push(jsonmap_to_hashmap(json_map));
+    match json_val {
+        Value::Array(json_array) => {
+            for jsonvalue in json_array {
+                filter_findmnt(&jsonvalue, filter, results);
             }
         }
-        // If the object has arrays, then the assumption is that they are arrays
-        // of objects.
-        for (_, jsonvalue) in json_map {
-            if jsonvalue.is_array() {
-                filter_findmnt(jsonvalue, filter, results);
+        Value::Object(json_map) => {
+            if let Some(value) = json_map.get(filter.key) {
+                if filter.value == value
+                    || filter.value == key_adjusted_value(filter.key, value)
+                {
+                    results.push(jsonmap_to_hashmap(json_map));
+                }
+            }
+            // If the object has arrays, then the assumption is that they are
+            // arrays of objects.
+            for (_, jsonvalue) in json_map {
+                if jsonvalue.is_array() {
+                    filter_findmnt(jsonvalue, filter, results);
+                }
             }
         }
-    }
+        _ => {
+            warn!("Unexpected json type");
+        }
+    };
 }
 
 /// findmnt executable name.
-const FINDMNT: &str = "findmnt";
+const FIND_MNT: &str = "findmnt";
 /// findmnt arguments, we only want source, target and filesystem type fields.
-const FINDMNT_ARGS: [&str; 3] = ["-J", "-o", "SOURCE,TARGET,FSTYPE"];
+const FIND_MNT_ARGS: [&str; 3] = ["-J", "-o", "SOURCE,TARGET,FSTYPE"];
 
 /// Execute the Linux utility findmnt, collect the json output,
 /// invoke the filter function and return the filtered results.
 fn findmnt(
     params: Filter,
 ) -> Result<Vec<HashMap<String, String>>, DeviceError> {
-    let output = Command::new(FINDMNT).args(&FINDMNT_ARGS).output()?;
+    let output = Command::new(FIND_MNT).args(&FIND_MNT_ARGS).output()?;
     if output.status.success() {
         let json_str = String::from_utf8(output.stdout)?;
         let json: Value = serde_json::from_str(&json_str)?;
